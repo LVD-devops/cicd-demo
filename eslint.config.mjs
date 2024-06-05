d74d9ce3067a1e8f3e50114474f4fef44dcf3042
dunglv <dunglv@rikkeisoft.com>
2024-06-05 17:01:00 +0700

    add build

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      // 'no-unused-vars': 'on',
+      // '@typescript-eslint/no-unused-vars': ['error'],
+      // '@typescript-eslint/explicit-function-return-type': ['warn'],
+      '@typescript-eslint/naming-convention': [
+        'error',
+        {
+          selector: 'variableLike',
+          format: ['camelCase'],
+        },
+        {
+          selector: 'property',
+          format: ['snake_case'],
+        },
+      ],
+    },
+  },
+];
--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-const world = 'world';
+const world = 'world fff             sdgsdgsdgs           dgsdgsdgsdg             dgsdgsdgsdgsdgsdg     dsgsdg  fsfsdgsdgsdg';
 
-export function hello(who: string = world): string {
+export function hello(who: string = world ): string {
+    const tH = {
+        t: 123,
+        hooT: 77
+    };
     return `Hello ${who}! `;
 }
