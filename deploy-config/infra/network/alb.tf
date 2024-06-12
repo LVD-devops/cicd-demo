66f2de04f928ccb29be018fd3f875f8ed309465f
dunglv <dunglv@rikkeisoft.com>
2024-06-12 18:04:35 +0700

    hide route53 and show dns after deploy

--- deploy-config/infra/network/alb.tf
+++ deploy-config/infra/network/alb.tf
@@ -14,15 +14,6 @@ resource "aws_internet_gateway" "main" {
   }
 }
 
-#resource "aws_subnet" "public" {
-#  vpc_id            = aws_vpc.main.id
-#  cidr_block        = "10.0.1.0/24"
-#  map_public_ip_on_launch = true
-#  tags = {
-#    Name = "${var.app_name}-subnet-public"
-#  }
-#}
-
 resource "aws_subnet" "public" {
   count                   = 2
   vpc_id                  = aws_vpc.main.id
@@ -138,4 +129,8 @@ output "ecs_security_group_id" {
 
 output "alb_target_group_arn" {
   value = aws_lb_target_group.main.arn
+}
+
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
 }
